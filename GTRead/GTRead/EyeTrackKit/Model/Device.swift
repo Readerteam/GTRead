bd76ffeb0fb4fe1352e3b01b7f83b75b334696e5
yangjie.95 <yangjie.95@bytedance.com>
2021-05-03 15:41:06 +0800

    修改设备模型

--- GTRead/GTRead/EyeTrackKit/Model/Device.swift
+++ GTRead/GTRead/EyeTrackKit/Model/Device.swift
@@ -11,12 +11,10 @@ import SceneKit
 import ARKit
 
 public enum DeviceType: String, CaseIterable {
-    case iPhone = "iPhone"
-    case iPad = "iPad"
-    case iPadLandscape = "iPad Landscape"
+    case iPad11 = "iPad11"
+    case iPad12 = "iPad12"
 }
 
-// デバイス情報保持クラス
 public class Device {
     public var type: DeviceType
     public var screenSize: CGSize
@@ -25,12 +23,20 @@ public class Device {
     public var node: SCNNode
     public var screenNode: SCNNode
     public var compensation: CGPoint
-
-    public init(screenSize: CGSize, screenPointSize: CGSize, compensation: CGPoint) {
-        self.type = .iPad
-        self.screenSize = screenSize
-        self.screenPointSize = screenPointSize
-        self.compensation = compensation
+    
+    
+    public init(type: DeviceType) {
+        self.type = type
+        switch type {
+        case DeviceType.iPad11:
+            self.screenSize = CGSize(width: 0.1785, height: 0.2476)
+            self.screenPointSize = UIScreen.main.bounds.size
+            self.compensation = CGPoint(x: 50, y: 50)
+        case DeviceType.iPad12:
+            self.screenSize = CGSize(width: 0.2149, height: 0.2806)
+            self.screenPointSize = UIScreen.main.bounds.size
+            self.compensation = CGPoint(x: 50, y: 50)
+        }
         // Node生成
         self.node = SCNNode()
         self.screenNode = {
@@ -43,34 +49,16 @@ public class Device {
         }()
         self.node.addChildNode(self.screenNode)
     }
-
-    public init(type: DeviceType) {
-        self.type = type
-        switch type {
-        case DeviceType.iPhone:
-            self.screenSize = CGSize(width: 0.0714, height: 0.1440)
-            self.screenPointSize = CGSize(width: 375, height: 812)
-            self.compensation = CGPoint(x: 0, y: 375)
-        case DeviceType.iPad:
-            self.screenSize = CGSize(width: 0.2149, height: 0.2806)
-            self.screenPointSize = CGSize(width: 1024, height: 1366)
-            self.compensation = CGPoint(x: 50, y: 50)
-        case DeviceType.iPadLandscape:
-            self.screenSize = CGSize(width: 0.2476, height: 0.1785)
-            self.screenPointSize = CGSize(width: 1194, height: 834)
-            self.compensation = CGPoint(x: 417, y: 0)
+    
+    // 获取机型尺寸函数
+    public static func getDeviceType() -> String {
+        switch UIScreen.main.bounds.size {
+        case CGSize(width: 834.0, height: 1194.0):
+            return DeviceType.iPad11.rawValue
+        case CGSize(width: 1024.0, height: 1366.0):
+            return DeviceType.iPad12.rawValue
+        default:
+            return ""
         }
-
-        // Node生成
-        self.node = SCNNode()
-        self.screenNode = {
-            let screenGeometry = SCNPlane(width: 1, height: 1)
-            screenGeometry.firstMaterial?.isDoubleSided = true
-            screenGeometry.firstMaterial?.diffuse.contents = UIColor.green
-            let vsNode = SCNNode()
-            vsNode.geometry = screenGeometry
-            return vsNode
-        }()
-        self.node.addChildNode(self.screenNode)
     }
 }
